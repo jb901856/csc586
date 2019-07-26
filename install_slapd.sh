8cb96eb9f4ffa0c0933cd104cc42f26ec9fc07fd
jb901856 <53063569+jb901856@users.noreply.github.com>
2019-07-25 21:55:35 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,1 +1,12 @@
+sudo su
 
+export DEBIAN_FRONTEND=noninteractive
+
+echo -e "slapd slapd/root_password password 123" |debconf-set-selections
+echo -e "slapd slapd/root_password_again password 123" |debconf-set-selections
+echo -e "slapd slapd/internal/adminpw password 123" |debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw password 123" |debconf-set-selections
+echo -e "slapd slapd/password2 password 123" |debconf-set-selections
+echo -e "slapd slapd/password1 password 123" |debconf-set-selections
+
+apt-get install ldap-utils slapd -y
