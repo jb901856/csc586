3957b8ef56d2ae4fbc882a14943b1d6a92d29e2e
jb901856 <53063569+jb901856@users.noreply.github.com>
2019-07-26 11:29:28 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -10,3 +10,19 @@ echo -e "slapd slapd/password2 password 123" |debconf-set-selections
 echo -e "slapd slapd/password1 password 123" |debconf-set-selections
 
 apt-get install ldap-utils slapd -y
+
+dpkg-reconfigure slapd
+
+ufw allow ldap
+
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -f basedn.ldif
+
+
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -F users.ldif
+
+cd clnodevm109-1.clemson.cloudlab.us
+
+sudo apt-get update
+
+sudo apt install -y libnss-ldap libpam-ldap ldap-utils
+
