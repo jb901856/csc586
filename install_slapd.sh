51d45017895247f4be3b9383119e798e7dab0df9
jb901856 <53063569+jb901856@users.noreply.github.com>
2019-07-30 12:34:41 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -2,19 +2,20 @@
 #log into ldapserver
 
 sudo apt-get update
+sudo apt-get install debconf-utils
 
-sudo echo -e "slapd slapd/root_password password 123" |debconf-set-selections
-sudo echo -e "slapd slapd/root_password_again password 123" |debconf-set-selections
-sudo echo -e "slapd slapd/internal/adminpw password 123" |debconf-set-selections
-sudo echo -e "slapd slapd/internal/generated_adminpw password 123" |debconf-set-selections
-sudo echo -e "slapd slapd/password2 password 123" |debconf-set-selections
-sudo echo -e "slapd slapd/password1 password 123" |debconf-set-selections
-sudo echo -e "slapd slapd/domain string clemson.cloudlab.us" |debconf-set-selections
-sudo echo -e "slapd slapd/move_old_databse boolean true" |debconf-set-selections
-sudo echo -e "slapd shared/organization string nodomain" |debconf-set-selections
-sudo echo -e "slapd slapd/purge_database boolean false" |debconf-set-selections
-sudo echo -e "slapd slapd/no_configuration boolean false" |debconf-set-selections
-sudo echo -e "slapd slapd/backend select MDB" |debconf-set-selctions
+echo -e "slapd slapd/root_password password 123" |debconf-set-selections
+echo -e "slapd slapd/root_password_again password 123" |debconf-set-selections
+echo -e "slapd slapd/internal/adminpw password 123" |debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw password 123" |debconf-set-selections
+echo -e "slapd slapd/password2 password 123" |debconf-set-selections
+echo -e "slapd slapd/password1 password 123" |debconf-set-selections
+echo -e "slapd slapd/domain string clemson.cloudlab.us" |debconf-set-selections
+echo -e "slapd slapd/move_old_databse boolean true" |debconf-set-selections
+echo -e "slapd shared/organization string nodomain" |debconf-set-selections
+echo -e "slapd slapd/purge_database boolean false" |debconf-set-selections
+echo -e "slapd slapd/no_configuration boolean false" |debconf-set-selections
+echo -e "slapd slapd/backend select MDB" |debconf-set-selections
 
 sudo export DEBIAN_FRONTEND=noninteractive apt-get install -y slapd ldap-utils
 
