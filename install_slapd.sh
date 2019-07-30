9c5b09231400a1bbf99d82b85c3f2bfd326c0431
jb901856 <53063569+jb901856@users.noreply.github.com>
2019-07-30 08:10:05 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,29 +1,30 @@
-sudo su
-apt-get update
-cd
-export DEBIAN_FRONTEND=noninteractive
+#!/bin/bash
+#log into ldapclient
 
-echo -e "slapd slapd/root_password password 123" |debconf-set-selections
-echo -e "slapd slapd/root_password_again password 123" |debconf-set-selections
-echo -e "slapd slapd/internal/adminpw password 123" |debconf-set-selections
-echo -e "slapd slapd/internal/generated_adminpw password 123" |debconf-set-selections
-echo -e "slapd slapd/password2 password 123" |debconf-set-selections
-echo -e "slapd slapd/password1 password 123" |debconf-set-selections
+sudo apt-get update
 
-apt-get install ldap-utils slapd -y
+sudo export DEBIAN_FRONTEND=noninteractive
 
-dpkg-reconfigure slapd
+sudo echo -e "slapd slapd/root_password password 123" |debconf-set-selections
+sudo echo -e "slapd slapd/root_password_again password 123" |debconf-set-selections
+sudo echo -e "slapd slapd/internal/adminpw password 123" |debconf-set-selections
+sudo echo -e "slapd slapd/internal/generated_adminpw password 123" |debconf-set-selections
+sudo echo -e "slapd slapd/password2 password 123" |debconf-set-selections
+sudo echo -e "slapd slapd/password1 password 123" |debconf-set-selections
+sudo echo -e "slapd slapd/domain string clemson.cloudlab.us" |debconf-set-selections
+sudo echo -e "slapd slapd/move_old_databse boolean true" |debconf-set-selections
+sudo echo -e "slapd shared/organization string nodomain" |debconf-set-selections
+sudo echo -e "slapd slapd/purge_database boolean false" |debconf-set-selections
+sudo echo -e "slapd slapd/no_configuration boolean false" |debconf-set-selections
+sudo echo -e "slapd slapd/backend select MDB" |debconf-set-selctions
 
-ufw allow ldap
+sudo apt-get install -y slapd ldap-utils
 
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -f basedn.ldif
+sudo dpkg-reconfigure slapd
+
+sudo ufw allow ldap
+
+sudo ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -f basedn.ldif
 
 
-#ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -F users.ldif
-
-#cd clnodevm109-1.clemson.cloudlab.us
-
-#sudo apt-get update
-
-#sudo apt install -y libnss-ldap libpam-ldap ldap-utils
-
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -F users.ldif
