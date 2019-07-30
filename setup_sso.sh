fb400cfc5d6d1481db57dfb15f80c8d3ef991c11
jb901856 <53063569+jb901856@users.noreply.github.com>
2019-07-30 19:04:41 -0400

    Update setup_sso.sh

--- setup_sso.sh
+++ setup_sso.sh
@@ -20,3 +20,18 @@ libnss-ldap libnss-ldap/nsswitch    note    \
 " | debconf-set-selections
 
 apt install -y libnss-ldap libpam-ldap ldap-utils
+
+sudo sed -i 's/uri ldapi:\/\/\//uri ldap:\/\/192.168.1.1\//g' /etc/ldap.conf
+sudo sed -i 's/base dc=example,dc=net/base dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
+sudo sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=admin,dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
+sudo sed -i '/passwd:/ s/$/ ldap/' /etc/nsswitch.conf
+sudo sed -i '/group:/ s/$/ ldap/' /etc/nsswitch.conf
+sudo sed -i '/# end of pam-auth-update config/ i session optional pam_mkhomedir.so  skel=/etc/skel  umsak=077' /etc/pam.d/common-session
+sudo sed -i 's/use_authtok//g' /etc/pam.d/common-password
+sudo bash <<EOF
+echo 123 > /etc/ldap.secret
+EOF
+sudo chmod 600 /etc/ldap.secret
+
+#fetches and prints details for a particular user 
+getent passwd student
