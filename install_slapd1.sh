2a0fc746b42cc3ece354863d0b35eb33dd6d014b
jb901856 <53063569+jb901856@users.noreply.github.com>
2019-08-01 09:24:21 -0400

    Update install_slapd1.sh

--- install_slapd1.sh
+++ install_slapd1.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-#log into ldapserver
-#please lof into sudo su in order to run
 
-apt-get update
+#By Jeff Bowker
+#csc586 Assignment 2
+#log into ldapserver
+
+sudo apt-get update
 
 export DEBIAN_FRONTEND=noninteractive
 
-echo -e "slapd slapd/root_password password 123" |debconf-set-selections
-echo -e "slapd slapd/root_password_again password 123" |debconf-set-selections
-echo -e "slapd slapd/internal/adminpw password 123" |debconf-set-selections
-echo -e "slapd slapd/internal/generated_adminpw password 123" |debconf-set-selections
-echo -e "slapd slapd/password2 password 123" |debconf-set-selections
-echo -e "slapd slapd/password1 password 123" |debconf-set-selections
-echo -e "slapd slapd/domain string clemson.cloudlab.us" |debconf-set-selections
-echo -e "slapd slapd/move_old_databse boolean true" |debconf-set-selections
-echo -e "slapd shared/organization string nodomain" |debconf-set-selections
-echo -e "slapd slapd/purge_database boolean false" |debconf-set-selections
-echo -e "slapd slapd/no_configuration boolean false" |debconf-set-selections
-echo -e "slapd slapd/backend select MDB" |debconf-set-selections
+echo -e "slapd slapd/root_password password 123" |sudo debconf-set-selections
+echo -e "slapd slapd/root_password_again password 123" |sudo debconf-set-selections
+echo -e "slapd slapd/internal/adminpw password 123" |sudo debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw password 123" |sudo debconf-set-selections
+echo -e "slapd slapd/password2 password 123" |sudo debconf-set-selections
+echo -e "slapd slapd/password1 password 123" |sudo debconf-set-selections
+echo -e "slapd slapd/domain string clemson.cloudlab.us" |sudo debconf-set-selections
+echo -e "slapd slapd/move_old_databse boolean true" |sudo debconf-set-selections
+echo -e "slapd shared/organization string nodomain" |sudo debconf-set-selections
+echo -e "slapd slapd/purge_database boolean false" |sudo debconf-set-selections
+echo -e "slapd slapd/no_configuration boolean false" |sudo debconf-set-selections
+echo -e "slapd slapd/backend select MDB" |sudo debconf-set-selections
 
-apt-get install ldap-utils slapd -q -y
+sudo apt-get install ldap-utils slapd -q -y
 
 sudo dpkg-reconfigure slapd
 
